a3c3bd1244f1da36e51c7c84043423c998b42112
Hoang Anh Tuan <hoangtuandhbk@gmail.com>
2021-01-17 16:41:59 +0700

    In progress

--- app/Jobs/UploadCustomCertificateToCloudflare.php
+++ app/Jobs/UploadCustomCertificateToCloudflare.php
@@ -12,6 +12,7 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use App\Events\UploadCustomCertificateToCloudflareCompleted;
 
 class UploadCustomCertificateToCloudflare implements ShouldQueue
 {
