5b013705579ab7ee087c32b0dda986241414f0bc
Hoang Anh Tuan <hoangtuandhbk@gmail.com>
2022-05-22 17:56:34 +0700

    Minor change

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('netcorelog:purge')->everyFiveMinutes();
+        $schedule->command('netcorelog:purge')->everyMinute();
     }
 
     /**
