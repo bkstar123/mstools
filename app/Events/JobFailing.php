3051b4f988e0f17ad695167a70ab8e060c1eedc0
Hoang Anh Tuan <hoangtuandhbk@gmail.com>
2022-07-29 00:02:36 +0700

    Minor remove unnecessary class importing

--- app/Events/JobFailing.php
+++ app/Events/JobFailing.php
@@ -2,11 +2,9 @@
 
 namespace App\Events;
 
-use Illuminate\Broadcasting\Channel;
 use Bkstar123\BksCMS\AdminPanel\Admin;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
